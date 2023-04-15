1ad4ecd9377d8b5401d4bb716a093965ab78438d
Cihan-Tiambeng <cihan.tiambeng@gmail.com>
2023-04-15 22:02:49 +0300

    Update mastermind.py
    Improve readability and follow conventions

--- mastermind.py
+++ mastermind.py
@@ -10,16 +10,18 @@ import functools
 import sys
 
 code = random.sample(["r", "r", "b", "b", "g", "g", "y", "y", "c", "c", "m", "m"], 4)
-i = 1
-while i <= 13:
-    guess = list(input("MasterMind " + str(i) + " < ").lower())
-    if len(guess) != 4: print("Error! Only 4 characters allowed!")
-    elif not all(char in ["r", "b", "g", "y", "c", "m"] for char in guess): print("Error! Only letters (r)ed (g)reen (b)lue (m)agenta (c)yan (y)ellow allowed!")
+turn = 1
+while turn <= 13:
+    guess = list(input(f"MasterMind {turn} < ").lower())
+    if len(guess) != 4: 
+        print("Error! Only 4 characters allowed!")
+    elif not all(char in ["r", "b", "g", "y", "c", "m"] for char in guess): 
+        print("Error! Only letters (r)ed (g)reen (b)lue (m)agenta (c)yan (y)ellow allowed!")
     else :
         b = sum(1 for i, j in enumerate(code) if j == guess[i])
         w = len([char for char in code if char in guess]) - b
-        print("b : " + str(b) + " w : " + str(w))
-        i+=1
+        print(f"b: {b} w: {w}")
+        turn += 1
         if(guess == code): 
             print("You Win!")
             sys.exit()
